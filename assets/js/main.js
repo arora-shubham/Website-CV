a9996e2115e39d6813ed82975dfbe1b0cd1f2da1
arora-shubham <shubham.arora868@outlook.com>
2016-03-22 21:15:42 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+if ('addEventListener' in window) {
+    window.addEventListener('load', function () {
+        document.body.className = document.body.className.replace(/\bloading\b/, '');
+    });
+    document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+    $(".social").addClass("animated zoomIn");
+    $("#resume").addClass("animated slideInUp");
+    $(".avatar").addClass("animated tada");
+    $("#hi").addClass("animated zoomInUp");
+    $("#main").addClass("animated flipInY");
+}
